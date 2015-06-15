dfd17110f7a6ae2f096c296c2b69dbbd8a395052
joeryhuiden <joeryhuiden@hotmail.com>
2015-06-15 14:44:50 +0200

    Simplyfied LogoutServlet

--- FamilyWeb/src/main/java/servlets/FamilyWeb/LogoutServlet.java
+++ FamilyWeb/src/main/java/servlets/FamilyWeb/LogoutServlet.java
@@ -2,32 +2,32 @@ package servlets.FamilyWeb;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LogoutServlet
  */
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	}
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException { }
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		RequestDispatcher reqDisp = null;		
-		req.setAttribute("message", "U bent succesvol uitgelogd.");
-		reqDisp = req.getRequestDispatcher("/login.jsp");
-		req.getSession().invalidate();		
-		reqDisp.forward(req, resp);
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		req.getSession().invalidate();
+		getServletContext().getRequestDispatcher("/login.jsp").forward(req, resp);
 	}
 }
