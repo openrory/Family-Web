632de2108caa93ae2ce54b3f4743c18c663aa4d2
joeryhuiden <joeryhuiden@hotmail.com>
2015-06-15 14:37:53 +0200

    Added: LogoutServlet v1 FamilyInformationServlet v1
    changed:
    LoginServlet
    ContactServlet
    Web.xml (added servlets and mappings)

--- FamilyWeb/src/main/java/servlets/FamilyWeb/LoginServlet.java
+++ FamilyWeb/src/main/java/servlets/FamilyWeb/LoginServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import domain.FamilyWeb.User;
 import servletControllers.FamilyWeb.LoginController;
 
 @SuppressWarnings("serial")
@@ -23,19 +24,19 @@ public class LoginServlet extends HttpServlet {
 		String username = req.getParameter("username").trim();
 		String password = req.getParameter("password").trim();
 		
-		if (controller.authentication(username, password)) {
-			
-			Object user = controller.getUser(username);
+		if (controller.authentication(username, password)) {			
+			User user = (User) controller.getUser(username);
 			req.getSession().setAttribute("user", user);
 			
 			if (controller.isAdministrator(user)) {
 				reqDisp = req.getRequestDispatcher("/administrator/startscreen_administrator.html");
 			} else {
+				req.getSession().setAttribute("clients", user.getDbController().getAllClientsOfUser(user));
 				reqDisp = req.getRequestDispatcher("/socialworker/startscreen_socialworker.html");
 			}
 		} else {
 			req.setAttribute("message", "Gebruikersnaam of wachtwoord onjuist.");
-			reqDisp = req.getRequestDispatcher("/login.html");
+			reqDisp = req.getRequestDispatcher("/login.jsp");
 		}
 		reqDisp.forward(req, resp);
 	}
---
+++ FamilyWeb/src/main/java/servlets/FamilyWeb/LogoutServlet.java
@@ -0,0 +1,33 @@
+package servlets.FamilyWeb;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher reqDisp = null;		
+		req.setAttribute("message", "U bent succesvol uitgelogd.");
+		reqDisp = req.getRequestDispatcher("/login.jsp");
+		req.getSession().invalidate();		
+		reqDisp.forward(req, resp);
+	}
+}
