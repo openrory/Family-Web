76c8948f1dddbc17c69e5150d9dbcd339e45dbe8
michielio <michiel.wilm@gmail.com>
2015-06-19 11:21:25 +0200

    nieuwe update aan javascript validations

--- FamilyWeb/src/main/webapp/javascripts/validate_add_edit_employee.js
+++ FamilyWeb/src/main/webapp/javascripts/validate_add_edit_employee.js
@@ -15,40 +15,46 @@ function validateForm() {
     var email = document.forms["add_form"]["email"].value;
     var email_confirmation = document.forms["add_form"]["email_confirmation"].value;
 
+    // Standar REGEX patterns
+    var digitsReg = /\d/g ;
+    var nonDigitsReg = /\D/g ;
     // Custom REGEX Patterns
-    var dateReg = /^\d{2}[./-]\d{2}[./-]\d{4}$/
-    var postcodeReg = /^\d{4}[-/ ]\D{2}$/
+    var dateReg = /^\d{2}[./-]\d{2}[./-]\d{4}$/ ;
+    var postcodeReg = /^\d{4}[-/ ]\D{2}$/       ;
     var emailReg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if (employeeid == null || employeeid == "" || employeeid == "/D") {
+
+
+
+    if (employeeid == null || employeeid == "" || nonDigitsReg.test(employeeid)) {
         document.getElementById("employeeidWarning").className = "false";
         isGood = false;
     } else {
         document.getElementById("employeeidWarning").className = "true";
     }
 
-    if (forename == null || forename == "" || forename == "/d") {
+    if (forename == null || forename == "" || digitsReg.test(forename)) {
         document.getElementById("forenameWarning").className = "false";
         isGood = false;
     } else {
         document.getElementById("forenameWarning").className = "true";
     }
 
-    if (surname == null || surname == "" || surname == "/d") {
+    if (surname == null || surname == "" || digitsReg.test(surname)) {
         document.getElementById("surnameWarning").className = "false";
         isGood = false;
     } else {
         document.getElementById("surnameWarning").className = "true";
     }
 
-    if (dateofbirth == null || dateofbirth == "" || dateofbirth.matches(dateReg)) {
+    if (dateofbirth == null || dateofbirth == "" || dateReg.test(dateofbirth)) {
         document.getElementById("dateofbirthWarning").className = "false";
         isGood = false;
     } else {
         document.getElementById("dateofbirthWarning").className = "true";
     }
 
-    if (nationality == null || nationality == "" || nationality == "/d") {
+    if (nationality == null || nationality == "" || digitsReg.test(nationality)) {
         document.getElementById("nationalityWarning").className = "false";
         isGood = false;
     } else {
@@ -62,49 +68,49 @@ function validateForm() {
         document.getElementById("streetWarning").className = "true";
     }
 
-    if (streetnumber == null || streetnumber == "" || streetnumber == "/D") {
+    if (streetnumber == null || streetnumber == "" || nonDigitsReg.test(streetnumber)) {
         document.getElementById("streetnumberWarning").className = "false";
         isGood = false;
     } else {
         document.getElementById("streetnumberWarning").className = "true";
     }
 
-    if (postcode == null || postcode == "" || postcode.matches(postcodeReg)) {
+    if (postcode == null || postcode == "" || postcodeReg.test(postcode)) {
         ////document.getElementById("postcoKdeWarning").className = "false";
         isGood = false;
     } else {
         document.getElementById("postcodeWarning").className = "true";
     }
 
-    if (city == null || city == "") {
+    if (city == null || city == "" || digitsReg.test(city)) {
         document.getElementById("cityWarning").className = "false";
         isGood = false;
     } else {
         document.getElementById("cityWarning").className = "true";
     }
 
-    if (phonenumber == null || phonenumber == "" || phonenumber == "/d" || phonenumber.length <= 15) {
+    if (phonenumber == null || phonenumber == "" || nonDigitsReg.test(phonenumber) || phonenumber.length > 15) {
         document.getElementById("phonenumberWarning").className = "false";
         isGood = false;
     } else {
         document.getElementById("phonenumberWarning").className = "true";
     }
     //
-    if (mobile == null || mobile == "" || mobile == "/d" || mobile.length <= 15) {
+    if (mobile == null || mobile == "" || nonDigitsReg.test(mobile) || mobile.length > 15) {
         document.getElementById("mobileWarning").className = "false";
         isGood = false;
     } else {
         document.getElementById("mobileWarning").className = "true";
     }
 
-    if (email == null || email == "" || email == email_confirmation || email.contains("@") || email.matches(emailReg)) {
+    if (email == null || email == "" || email != email_confirmation || email.contains("@") || !emailReg.test(email)) {
         document.getElementById("emailWarning").className = "false";
         isGood = false;
     } else {
         document.getElementById("emailWarning").className = "true";
     }
 
-    if (email_confirmation == null || email_confirmation == "" || email_confirmation == email || email_confirmation.contains("@") || email.matches(emailReg)) {
+    if (email_confirmation == null || email_confirmation == "" || email_confirmation != email || !emailReg.test(email_confirmation)) {
         document.getElementById("email_confirmationWarning").className = "false";
         isGood = false;
     } else {
