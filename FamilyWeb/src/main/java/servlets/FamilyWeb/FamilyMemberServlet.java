68741f1ffabbdcc411134081f03d45d1aa14a96f
joeryhuiden <joeryhuiden@hotmail.com>
2015-06-24 23:21:06 +0200

    added first steps for adding or update an familymember

---
+++ FamilyWeb/src/main/java/servlets/FamilyWeb/FamilyMemberServlet.java
@@ -0,0 +1,93 @@
+package servlets.FamilyWeb;
+
+import java.io.IOException;
+import java.sql.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+
+import servletControllers.FamilyWeb.OverviewController;
+import domain.FamilyWeb.Client;
+import domain.FamilyWeb.Familymember;
+import domain.FamilyWeb.User;
+
+/**
+ * Servlet implementation class FamilyMemberServlet
+ */
+public class FamilyMemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;   
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher reqDisp = null;
+		String option = (req.getParameter("option") != null) ? (String) req.getParameter("option") : "";
+		Client client = (Client) req.getSession().getAttribute("client");
+		
+		if(option.equals("Aanmaken")){
+			String forename = req.getParameter("forename");
+			String surname = req.getParameter("surname");
+			String dateOfBirthString = req.getParameter("dateofbirth");
+			Date dateOfBirth = null;
+			String postcode = req.getParameter("postcode");
+			String street = req.getParameter("street");
+			String houseNumber = req.getParameter("streetnumber");
+			String city = req.getParameter("city");
+			String nationality = req.getParameter("nationality");
+			String telephoneNumber = req.getParameter("phonenumber");
+			String mobilePhoneNumber = req.getParameter("mobile");
+			String email = req.getParameter("email");
+			
+			Familymember fm = new Familymember(forename, surname, dateOfBirth, postcode, street, houseNumber, city, nationality, telephoneNumber, mobilePhoneNumber, email);
+			OverviewController.getInstance().getDb().addFamilymember(fm, client);
+			req.setAttribute("message", "Gezinslid Aangemaakt.");
+			req.setAttribute("messageType", "succes");
+			reqDisp = req.getRequestDispatcher("/socialworker/family/family_members_overview.jsp");
+		}else if(option.equals("Opslaan")){
+			int id = Integer.valueOf(req.getParameter("familymemberID"));
+			String forename = req.getParameter("forename");
+			String surname = req.getParameter("surname");
+			String dateOfBirthString = req.getParameter("dateofbirth");
+			Date dateOfBirth = null;
+			String postcode = req.getParameter("postcode");
+			String street = req.getParameter("street");
+			String houseNumber = req.getParameter("streetnumber");
+			String city = req.getParameter("city");
+			String nationality = req.getParameter("nationality");
+			String telephoneNumber = req.getParameter("phonenumber");
+			String mobilePhoneNumber = req.getParameter("mobile");
+			String email = req.getParameter("email");
+			Familymember fm = new Familymember(forename, surname, dateOfBirth, postcode, street, houseNumber, city, nationality, telephoneNumber, mobilePhoneNumber, email);
+			fm.setMember_id(id);
+			OverviewController.getInstance().getDb().updateFamilymember(fm);
+			req.setAttribute("message", "Gezinslid opgeslagen.");
+			req.setAttribute("messageType", "succes");
+			reqDisp = req.getRequestDispatcher("/socialworker/family/family_members_overview.jsp");
+		}else{			
+			req.setAttribute("message", "Onverwachte fout opgetreden, pagina niet gevonden.");
+			req.setAttribute("messageType", "error");
+			reqDisp = req.getRequestDispatcher("/socialworker/family/family_members_overview.jsp");
+		}
+		try {
+			req.getSession().setAttribute("familyJSON", OverviewController.getInstance().refreshFamilymember(client));
+		} catch (JSONException e) {
+			req.setAttribute("message", "Kan degegevens van "+client.getForename()+" "+client.getSurname()+" niet goed ophalen, log opnieuw in en probeer het opnieuw.");
+			req.setAttribute("messageType", "error");
+			reqDisp = req.getRequestDispatcher("/socialworker/startscreen_socialworker.jsp");
+		}
+		reqDisp.forward(req, resp);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+}
